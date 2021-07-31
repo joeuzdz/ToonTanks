6bc4d63b371d94bf58613cb1feef5e4688ad4ace
Joe Uzdzinski <juzdzinski@gmail.com>
2021-07-31 18:20:22 -0400

    Added PawnBaseComponents

--- Source/ToonTanks/Pawns/PawnBase.h
+++ Source/ToonTanks/Pawns/PawnBase.h
@@ -6,24 +6,36 @@
 #include "GameFramework/Pawn.h"
 #include "PawnBase.generated.h"
 
+class UCapsuleComponent;
+
 UCLASS()
 class TOONTANKS_API APawnBase : public APawn
 {
 	GENERATED_BODY()
 
 public:
+
 	// Sets default values for this pawn's properties
 	APawnBase();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+protected:
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:	
+	UPROPERTY()
+	UCapsuleComponent* CapsuleComp = nullptr;
+	UPROPERTY()
+	UStaticMeshComponent* BaseMesh = nullptr;
+	UPROPERTY()
+	UStaticMeshComponent* TurretMesh = nullptr;
+	UPROPERTY()
+	USceneComponent* ProjectileSpawnPoint = nullptr;
 };
